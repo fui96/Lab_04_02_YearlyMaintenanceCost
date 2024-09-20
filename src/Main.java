52055a78c3954dd53dd13ca40e1849edd7f88d9e
fui96 <kidwelqn@mail.uc.edu>
2024-09-20 16:32:58 -0400

    innit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int SummerCost = 100;
+        int FallCost = 110;
+        int WinterCost = 150;
+        int SpringCost = 90;
+        int YearCost;
+
+        System.out.println("Summer Cost: " + SummerCost);
+        System.out.println("Fall Cost: " + FallCost);
+        System.out.println("Winter Cost: " + WinterCost);
+        System.out.println("Spring Cost: " + SpringCost);
+        YearCost = SummerCost + FallCost + WinterCost + SpringCost;
+        System.out.println("Year Cost: " + YearCost);
+
+
+        }
+    }
